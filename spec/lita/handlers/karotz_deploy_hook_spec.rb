c9756316382c31407cc3be5c6739c163a854d0e8
Mathieu Gilbert <matnarak@gmail.com>
2014-07-22 12:21:34 -0600

    Allowing for spaces in the parameter values (ie. deployer name)

--- spec/lita/handlers/karotz_deploy_hook_spec.rb
+++ spec/lita/handlers/karotz_deploy_hook_spec.rb
@@ -5,12 +5,12 @@ describe Lita::Handlers::KarotzDeployHook, lita_handler: true do
   it 'posts data to the Karotz deployments endpoint' do
     Lita.config.handlers.karotz_deploy_hook.karotz_deployment_endpoint = 'http://www.google.com/deployments'
     expect(RestClient).to receive(:post)
-    send_message "teddy is starting deploy of 't_y_l_f' from branch 'ice-cream' to production"
+    send_message "Teddy Ruxbin is starting deploy of 't_y_l_f' from branch 'ice-cream' to production"
   end
 
   it 'throws an exception when endpoint not set' do
     expect {
-      send_message "teddy is starting deploy of 't_y_l_f' from branch 'ice-cream' to production"
+      send_message "Teddy Ruxbin is starting deploy of 't_y_l_f' from branch 'ice-cream' to production"
     }.to raise_error
   end
 end
